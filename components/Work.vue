8c777e9e21bc6209ed726b05fbb3026fe68b9461
nicholas calderone <nick@nicholass-MacBook-Air.local>
2023-01-25 18:41:25 -0700

    dropped h2 font size on tablet and mobile

--- components/Work.vue
+++ components/Work.vue
@@ -1,35 +1,116 @@
 <script>
 export default {
+	data() {
+		return {
+			parent: undefined,
+			canvas: undefined,
+			ctx: undefined,
+			lineWidth: undefined,
+			radius: undefined,
+			color: undefined,
+			lineCap: "round"
+		}
+	},
 	mounted() {
-		let canvas = document.querySelector("#work-canvas");
-		let context = canvas.getContext("2d");
+		this.parent = document.querySelector(".work-canvas-wrapper");
+		this.canvas = document.querySelector("#work-canvas");
+		this.ctx = this.canvas.getContext("2d");
+		this.ctx.lineCap = "round";
+		this.color = getComputedStyle(document.documentElement).getPropertyValue("--color-secondary");
+		this.breakpoint = getComputedStyle(document.documentElement).getPropertyValue("--breakpoint");
 
-		// make canvas the same size as container
-		var rect = canvas.parentNode.getBoundingClientRect();
-		canvas.width = rect.width;
-		canvas.height = rect.height;
+		let rect = this.parent.getBoundingClientRect();
+		let w = rect.width;
+		let h = rect.height;
 
-		let width = rect.width;
-		let height = rect.height;
 
-		let offset = 2.5;
-		let radius = 12;
+		this.draw(w, h);
 
-		context.lineWidth = 5;
-		context.lineCap = "round";
+		const observer = new ResizeObserver(entries => {
 
-		context.beginPath();
-		context.moveTo(width / 2, offset);
-		context.lineTo(radius + offset, offset);
-		context.arcTo(offset, offset, offset, radius + offset, radius);
-		context.lineTo(offset, height - (radius + offset));
-		context.arcTo(offset, height - offset, radius + offset, height - offset, radius);
-		context.lineTo(Math.round(width * .84), height - offset);
+			let w = entries[0].contentRect.width;
+			let h = entries[0].contentRect.height;
 
-		let secondaryColor = getComputedStyle(document.documentElement).getPropertyValue("--color-secondary");
-		context.strokeStyle = secondaryColor;
+			this.draw(w, h);
+		});
 
-		context.stroke();
+		observer.observe(this.parent);
+	},
+	methods: {
+		draw(w, h) {
+			if (window.matchMedia("(max-width:" + this.breakpoint + ")").matches) {
+				this.drawMobile(w, h);
+			} else {
+				this.drawDesktop(w, h);
+			}
+		},
+		getContext(w, h, lineWidth, radius) {
+
+			// setup canvas context
+			this.canvas.width = w;
+			this.canvas.height = h;
+			this.lineWidth = lineWidth;
+			this.radius = radius;
+
+			// setup stroke properties
+			this.ctx.lineWidth = this.lineWidth;
+			this.ctx.strokeStyle = this.color;
+			this.ctx.lineCap = this.lineCap;
+
+			return this.ctx;
+
+		},
+		drawDesktop(w, h) {
+
+			// set up and get canvas context
+			let context = this.getContext(w, h, 5, 12);
+
+			// get drawing variables
+			let width = this.canvas.width;
+			let height = this.canvas.height;
+
+			let offset = this.lineWidth / 2;
+			let radius = this.radius;
+
+			// draw directions
+			context.beginPath();
+			context.moveTo(width / 2, offset);
+			context.lineTo(radius + offset, offset);
+			context.arcTo(offset, offset, offset, radius + offset, radius);
+			context.lineTo(offset, height - (radius + offset));
+			context.arcTo(offset, height - offset, radius + offset, height - offset, radius);
+			context.lineTo(Math.round(width * .84), height - offset);
+
+			// draw
+			context.stroke();
+
+		},
+		drawMobile(w, h) {
+			// set up and get canvas context
+			let context = this.getContext(w, h, 5, 12);
+
+			// get drawing variables
+			let width = this.canvas.width;
+			let height = this.canvas.height;
+
+			let offset = this.lineWidth / 2;
+			let radius = this.radius;
+
+			let spacingVariable = getComputedStyle(document.documentElement).getPropertyValue("--spacing")
+
+			// remove units and change to a number
+			let spacing = Number(spacingVariable.slice(0, -2));
+
+			// draw directions
+			context.beginPath();
+			context.moveTo(width - offset, offset);
+			context.lineTo(width * .15 + radius + offset, offset);
+			context.arcTo(width * .15 + offset, offset, width * .15 + offset, offset + radius, radius);
+			context.lineTo(width * .15 + offset, height - offset - 20);
+
+			context.stroke();
+
+		}
 	}
 }
 
@@ -124,5 +205,47 @@ export default {
 .work-tools {
 	grid-row: 5;
 	grid-column: 1 / 7;
+}
+
+@media only screen and (max-width: 700px){
+	
+	.work-header-wrapper h2 {
+		text-align: center;
+	}
+
+	.work-wrapper {
+		grid-template-rows: auto 250px 40px 250px 40px 250px auto;
+		grid-template-columns: 1fr;
+	}
+
+	.work-canvas-wrapper {
+		grid-column: 1;
+		grid-row: 1 / 7;
+	}
+
+	.work-subheader {
+		grid-column: 1;
+		grid-row: 1;
+	}
+
+	.example-1 {
+		grid-column: 1;
+		grid-row: 2;
+	}
+
+	.example-2 {
+		grid-column: 1;
+		grid-row: 4;
+	}
+
+	.example-3 {
+		grid-column: 1;
+		grid-row: 6;
+	}
+
+	.work-tools {
+		grid-column: 1;
+		grid-row: 7;
+	}
 }
 </style>
