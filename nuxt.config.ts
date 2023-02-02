6018f9e08282d91689eab655404c16f0c946bafd
nicholas calderone <nick@nicholass-MacBook-Air.local>
2023-02-02 12:33:37 -0700

    updated meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,8 @@ export default defineNuxtConfig({
 			meta: [
 				{ name: "viewport", content: "width=device-width, initial-scale=1" },
 				{ charset: "utf-8" },
-				{ "http-equiv": "X-UA-Compatible", content: "IE=edge" }
+				{ "http-equiv": "X-UA-Compatible", content: "IE=edge" },
+				{ name: "description", content: "Nick Calderone is a front-end developer who builds intuitive, engaging, & accessible interfaces for the web." }
 			],
 			link: [
 				{ rel: "icon", type: "image/svg", sizes: "any", href: "logo-icon.svg" }
