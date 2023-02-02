4623e25ca9c4341fbb85d529546c8c7f20a07daa
nicholas calderone <nick@nicholass-MacBook-Air.local>
2023-02-02 12:29:58 -0700

    added aria-label to button

--- components/Hamburger.vue
+++ components/Hamburger.vue
@@ -4,7 +4,7 @@ export default {
 }
 </script>
 <template>
-	<button class="mobile-menu-button" @click="menuHandler">
+	<button class="mobile-menu-button" aria-label="menu" @click="menuHandler">
 		<svg width="60" height="60" viewBox="0 0 100 100">
 			<path class="line line1"
 				stroke-linecap="round" d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058" />
