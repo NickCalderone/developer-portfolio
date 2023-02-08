a22454b13c878e156d509603c7b259e50ae3a5ab
nicholas calderone <nick@nicholass-MacBook-Air.local>
2023-02-08 15:50:54 -0700

    added link and open in new tab icon for this site repository

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -3,7 +3,7 @@
 		<Content colorL="var(--color-stripe-medium)" colorM="var(--color-stripe-light)"
 			colorR="var(--color-stripe-medium)">
 			<div class="footer">
-				<p>&copy; 2022-2023 Designed & Built by Nick Calderone / <a href="tel:+19709893545">970.989.3545</a> /
+				<p><a href="https://github.com/NickCalderone/developer-portfolio" rel="noopener noreferrer" target="_blank">Designed & Built by Nick Calderone</a><NewTabIcon /> / <a href="tel:+19709893545">970.989.3545</a> /
 					<a href="mailto:calderonenick@gmail.com">calderonenick@gmail.com</a></p>
 			</div>
 		</Content>
