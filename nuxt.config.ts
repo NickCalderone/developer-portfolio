86325b3a477788094a38f0008edca46715bffedd
NickCalderone <calderonenick@gmail.com>
2023-09-14 09:23:50 -0600

    updated meta description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
 				{ name: "viewport", content: "width=device-width, initial-scale=1" },
 				{ charset: "utf-8" },
 				{ "http-equiv": "X-UA-Compatible", content: "IE=edge" },
-				{ name: "description", content: "Nick Calderone is a front-end developer who builds intuitive, engaging, & accessible interfaces for the web." }
+				{ name: "description", content: "Nick Calderone | Front-end web developer based in Carbondale, Colorado | Building intuitive, engaging, accessible interfaces for businesses and municipalities" }
 			],
 			link: [
 				{ rel: "icon", type: "image/svg", sizes: "any", href: "logo-icon.svg" }
